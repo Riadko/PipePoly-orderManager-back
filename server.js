603ed03cd80dca6bd2822f18830e828fc8d4c002
KOLLI Riad <riad.kolli@etu.univ-lehavre.fr>
2025-02-18 15:05:13 +0100

    correction pour l'erreur NotFound

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const ordersRoutes = require('./routes/orders');
-
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +9,14 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/orders', ordersRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Catch-all handler to redirect all other requests to the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
